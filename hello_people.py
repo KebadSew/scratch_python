79e8bd57e70c883f6a6c7824d80efc00214a12a2
Mike Ayele <lingering.quest@gmail.com>
2020-09-05 12:24:56 -0400

    Uploaded Few Exercise Files

---
+++ hello_people.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 27 21:52:18 2020
+
+@author: legen
+"""
+
+"""
+This is sample program which will output 'Hello World!'
+"""
+
+print("Hello My People!")
+print("1+1 =",1+1)
+print("2*3 =",2*3)
+print("4/2 =",4/2)
+
+x = 5
+y = 3
+z = x + y
+
+print('The sum of 5 + 3 =', z)
+
+""" 
+this is a comment
+"""
