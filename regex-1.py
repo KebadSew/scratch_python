829a613cd9edce008ab45c1bf342a77561986f80
Mike Ayele <lingering.quest@gmail.com>
2020-12-28 20:34:05 -0500

    Update regex-1.py

--- regex-1.py
+++ regex-1.py
@@ -44,3 +44,33 @@ fname_pattern = r'[A-Z]{1}[a-z]+'
 fname_regex = re.compile(fname_pattern)
 print(fname_regex.findall(text))
 
+
+import re
+
+text = """Dave dave@google.com
+    Steve steve@gmail.com
+    Rob rob@gmail.com
+    Ryan ryan@yahoo.com
+    """
+pattern = r'[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}'
+
+# re.IGNORECASE makes the regex case-insensitive
+regex = re.compile(pattern, flags=re.IGNORECASE)
+print(regex.findall(text))
+m =regex.search(text)
+email = text[m.start():m.end()]
+
+print(email)
+
+
+
+# text = """Dave dave@google.com
+# Steve steve@gmail.com
+# Rob rob@gmail.com
+# Ad adem@gmail.com
+# Ryan ryan@yahoo.com"""
+#
+# fname_pattern = r'[A-Z]{1}[a-z]+'
+# fname_regex = re.compile(fname_pattern)
+# print(fname_regex.findall(text))
+
